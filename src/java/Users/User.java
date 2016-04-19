ac5e49adea3477db8c3c437fefa7e3c2641245e7
yixxie <yi.xie.1@stonybrook.edu>
2016-04-19 17:25:22 -0400

    register

--- src/java/Users/User.java
+++ src/java/Users/User.java
@@ -1,6 +1,8 @@
 
 package Users;
 
+import java.util.regex.Pattern;
+
 public abstract class User {
     
     public enum Role {
@@ -10,15 +12,49 @@ public abstract class User {
     Role role;
     Long userId;
     String username;
-    Byte[] password;
+    String password; //might change later
+    String rePassword;
     String firstName;
     String lastName;
+    String email;
     Address address;
     
-    public int deleteAccount() {
-        return 0;
+    public void setUserName(String username){
+        this.username=username;
     }
-    
+    public String getUserName(){
+        return username;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setRePassword(String rePassword){
+        this.rePassword=rePassword;
+    }
+    public String getRePassword(){
+        return rePassword;
+    }
+    public void setFirstName(String firstName){
+        this.firstName=firstName;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName=lastName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getEmail(){
+        return email;
+    }
     public int sendMessage() {
         return 0;
     }
