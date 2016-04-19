25cc8a161302e73ae9c2c175ea4e5ad2dbeaabc8
yiulin <yiulin@172.24.164.75>
2016-04-19 19:37:04 -0400

    +Item/user edits

--- src/java/Items/EBook.java
+++ src/java/Items/EBook.java
@@ -1,18 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Items;
 
 import java.io.Serializable;
 
-/**
- *
- * @author yiulin
- */
 public class EBook extends Item implements Serializable {
     
+    
+    String author;
+    int pages;
+    
+    public EBook() {
+        super();
+    }
     //Test bean only, needs to be changed later
      public String[] getList(){
         String[] list={"images/book1.jpg","images/book2.jpg","images/book3.jpg","images/book4.jpg","images/book5.jpg","images/book6.jpg"};
--- src/java/Items/Item.java
+++ src/java/Items/Item.java
@@ -1,22 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Items;
 
-/**
- *
- * @author yiulin
- */
+import java.util.Date;
+
+
 public abstract class Item {
     
     public enum Type {
         Ebook, AudioBook, Movie
     }
     
+    public Item() {
+        
+    }
+    
     Type type;
-    Long itemId;
+    Long ISBN;
+    String title;
+    String description;
+    String publisher;
+    Date releaseDate;
+    // URL imageURL;
+    //genre
     double averageRating;
     int totalCopies;
     int availableCopies;
