d9c7d43e2537cc53249b8852294ad378343a28d4
Brian Lee <brianleeof1994@gmail.com>
2016-04-22 14:27:10 -0400

    jquery instead of javascript for stars

--- web/js/custom.js
+++ web/js/custom.js
@@ -4,73 +4,29 @@
  * and open the template in the editor.
  */
 
-function hoverReview(value){
-    switch (value){
-        case 1:
-            document.getElementById("ratingStar1").src="images/filledStar.png";
-            break;
-        case 2:
-            document.getElementById("ratingStar1").src="images/filledStar.png";
-            document.getElementById("ratingStar2").src="images/filledStar.png";
-            break;
-        case 3:
-            document.getElementById("ratingStar1").src="images/filledStar.png";
-            document.getElementById("ratingStar2").src="images/filledStar.png";
-            document.getElementById("ratingStar3").src="images/filledStar.png";
-            break;
-        case 4:
-            document.getElementById("ratingStar1").src="images/filledStar.png";
-            document.getElementById("ratingStar2").src="images/filledStar.png";
-            document.getElementById("ratingStar3").src="images/filledStar.png";
-            document.getElementById("ratingStar4").src="images/filledStar.png";
-            break;
-        default:
-            document.getElementById("ratingStar1").src="images/filledStar.png";
-            document.getElementById("ratingStar2").src="images/filledStar.png";
-            document.getElementById("ratingStar3").src="images/filledStar.png";
-            document.getElementById("ratingStar4").src="images/filledStar.png";
-            document.getElementById("ratingStar5").src="images/filledStar.png";
-}
-}
-
-function unhoverReview(value){
-    switch (value){
-        case 1:
-            document.getElementById("ratingStar1").src="images/emptyStar.png";
-            break;
-        case 2:
-            document.getElementById("ratingStar1").src="images/emptyStar.png";
-            document.getElementById("ratingStar2").src="images/emptyStar.png";
-            break;
-        case 3:
-            document.getElementById("ratingStar1").src="images/emptyStar.png";
-            document.getElementById("ratingStar2").src="images/emptyStar.png";
-            document.getElementById("ratingStar3").src="images/emptyStar.png";
-            break;
-        case 4:
-            document.getElementById("ratingStar1").src="images/emptyStar.png";
-            document.getElementById("ratingStar2").src="images/emptyStar.png";
-            document.getElementById("ratingStar3").src="images/emptyStar.png";
-            document.getElementById("ratingStar4").src="images/emptyStar.png";
-            break;
-        default:
-            document.getElementById("ratingStar1").src="images/emptyStar.png";
-            document.getElementById("ratingStar2").src="images/emptyStar.png";
-            document.getElementById("ratingStar3").src="images/emptyStar.png";
-            document.getElementById("ratingStar4").src="images/emptyStar.png";
-            document.getElementById("ratingStar5").src="images/emptyStar.png";
-}
-}
-
 $(document).ready(function(){
     $(".ratingStars").children("#ratingStar").click(function(){	
-	alert($(this).attr("value") + "Stars!");
+	alert($(this).attr("value") + " Stars!");
 	});
 });
 
 $(document).ready(function(){
-    $(".ratingStars").children("#ratingStar").mouseenter(function(){	
-	$( "div" ).each(function( i ) {
+    $(".ratingStars").children("#ratingStar").mouseover(function(){
+        var starAmount = $(this).attr("value");
+        starAmount++;
+        for (var i = 1; i <starAmount; i++){
+            $(".ratingStars :nth-child(" + i + ")").attr("src","images/filledStar.png")
+        }
+
 	});
-})
+});
+
+$(document).ready(function(){
+    $(".ratingStars").children("#ratingStar").mouseleave(function(){
+        var starAmount = $(this).attr("value");
+        starAmount++;
+        for (var i = 1; i <starAmount; i++){
+            $(".ratingStars :nth-child(" + i + ")").attr("src","images/emptyStar.png");
+        }
+	});
 });
